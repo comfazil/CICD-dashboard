5f6e0b9ae3f9ff4d5d3941430b8f9aff930ee456
fazil <comfazil@yahoo.com>
2025-05-03 07:39:05 +0530

    Adding modified bash script that will execute log_analyzer.py first and then will perform the log cleanup.

--- cleanup_logs.sh
+++ cleanup_logs.sh
@@ -4,6 +4,19 @@ PROJECT_DIR="/home/fazilk/Desktop/DevOps"
 LOG_DIR="$PROJECT_DIR/logs"
 MAX_DAYS=7
 PROJECT_LOG="$PROJECT_DIR/project.log"
+LOG_ANALYZER="$PROJECT_DIR/log_analyzer.py"
+
+mkdir -p "$LOG_DIR"
+
+echo "[INFO] Running log analysis..."
+if [ -f "$LOG_ANALYZER" ]; then
+	python3 "$LOG_ANALYZER" || {
+	echo "[ERROR] Log analysis failed! Check for errors."
+	exit 1
+	}
+else
+	echo "[WARNING] log_analyzer.py not found. Skipping analysis."
+fi
 
 echo "[INFO] Cleaning up logs older than $MAX_DAYS days in $LOG_DIR"
 find $LOG_DIR -type f -name "*.log" -mtime +$MAX_DAYS -delete
